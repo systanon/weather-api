bd87db094e40db58442457fdd29316554f975466
Serhii <tustanovskiis@gmail.com>
2025-08-05 10:29:30 +0200

    feat: error handling

--- main.py
+++ main.py
@@ -4,28 +4,60 @@ import httpx
 
 
 async def fetchGeocode(city):
-    url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}"
-    async with httpx.AsyncClient(timeout=10) as client:
-        resp = await client.get(url)
-        data = resp.json()
-        if data.get("results"):
-            res = data["results"][0]
+    try:
+        url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}"
+        async with httpx.AsyncClient(timeout=10) as client:
+            resp = await client.get(url)
+            data = resp.json()
+            if data.get("results"):
+                res = data["results"][0]
+                return {
+                    "error": False,
+                    "latitude": res["latitude"],
+                    "longitude": res["longitude"],
+                }
             return {
-                "error": False,
-                "latitude": res["latitude"],
-                "longitude": res["longitude"],
+                "error": True,
             }
-        return {
-            "error": True,
-        }
+
+    except httpx.ConnectTimeout:
+        print("Timeout error: the request took too long.")
+        return {"error": True}
+
+    except httpx.ReadTimeout:
+        print("Reading response took too long!")
+
+    except httpx.ConnectError:
+        print("Connection error: could not reach the server.")
+        return {"error": True}
+
+    except httpx.RequestError as e:
+        print(f"Request error: {e}")
+        return {"error": True}
 
 
 async def fetchWeather(lat, lon):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
-    async with httpx.AsyncClient(timeout=10) as client:
-        resp = await client.get(url)
-        data = resp.json()
-        return data
+    try:
+        url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+        async with httpx.AsyncClient(timeout=10) as client:
+            resp = await client.get(url)
+            data = resp.json()
+            return data
+
+    except httpx.ConnectTimeout:
+        print("Timeout error: the request took too long.")
+        return {"error": True}
+
+    except httpx.ReadTimeout:
+        print("Reading response took too long!")
+
+    except httpx.ConnectError:
+        print("Connection error: could not reach the server.")
+        return {"error": True}
+
+    except httpx.RequestError as e:
+        print(f"Request error: {e}")
+        return {"error": True}
 
 
 async def processCity(city):
